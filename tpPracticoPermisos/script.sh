63b1df2d9791be6bd1b489550798cf6a1497ada7
JulioCesarRollano <lito9rollano.v@gmail.com>
2025-03-08 09:59:35 -0400

    Este es un documento de practico permisos

---
+++ tpPracticoPermisos/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Pedir el nombre del usuario
+echo "Por favor, ingrese su nombre:"
+read nombre
+
+# Saludar al usuario
+echo "Hola, $nombre. ¡Bienvenido!"
+
+# Imprimir el contenido del script
+echo "Contenido de script.sh:"
+cat script.sh
